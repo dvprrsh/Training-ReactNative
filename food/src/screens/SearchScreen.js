8355ab3ed67b1bdb6f557e915b92e2e985106dff
David <david--parrish@outlook.com>
2019-08-28 15:26:05 +0100

    Done

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { Text, StyleSheet } from 'react-native';
 import { SearchBar } from '../components/SearchBar';
+import { useResults } from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+import { COST_EFFECTIVE, BIT_PRICIER, BIG_SPENDER, MASSIVE_SPENDER } from '../constants';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  console.log('log', searchTerm);
+  const [searchApi, results, error] = useResults();
+
+  const filterResults = price => results.filter(result => result.price === price);
 
   return (
-    <View>
-      <SearchBar value={searchTerm} onInputChange={term => setSearchTerm(term)} />
-      <Text>{searchTerm}</Text>
-    </View>
+    <>
+      <SearchBar
+        value={searchTerm}
+        onInputChange={setSearchTerm}
+        onSubmit={() => searchApi(searchTerm)}
+      />
+      {error === '' ? null : <Text>{error}</Text>}
+      <ScrollView>
+        <ResultsList headerText="Cost Effective" filteredResults={filterResults(COST_EFFECTIVE)} />
+        <ResultsList headerText="Bit Pricier" filteredResults={filterResults(BIT_PRICIER)} />
+        <ResultsList headerText="Big Spender" filteredResults={filterResults(BIG_SPENDER)} />
+        <ResultsList
+          headerText="Massive Spender"
+          filteredResults={filterResults(MASSIVE_SPENDER)}
+        />
+      </ScrollView>
+    </>
   );
 };
 
