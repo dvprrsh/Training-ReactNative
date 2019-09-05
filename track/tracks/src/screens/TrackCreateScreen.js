ccac4b5f17db42cbb5fff545504162c838c42e9c
David <david--parrish@outlook.com>
2019-09-05 14:50:17 +0100

    File

--- track/tracks/src/screens/TrackCreateScreen.js
+++ track/tracks/src/screens/TrackCreateScreen.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
+import { Text } from 'react-native-elements';
+import { Map, TrackForm } from '../components';
+import { Context as LocationContext } from '../contexts/LocationContext';
+import useLocation from '../hooks/useLocation';
 
-export const TrackCreateScreen = () => (
-  <View>
-    <Text>TrackCreateScreen</Text>
-  </View>
-);
+import '../_mockLocation';
+
+const TrackCreateScreen = ({ isFocused }) => {
+  const { addLocation } = useContext(LocationContext);
+  const [permissionError] = useLocation(isFocused, addLocation);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <View>
+        <Text h4>Create Track</Text>
+        <Map />
+        {!permissionError ? <TrackForm /> : <Text>Please enable location services</Text>}
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({});
+
+export default withNavigationFocus(TrackCreateScreen);
